8a1c93c73a55e6e4d9caffbe37bbe4637eb95a4c
romanaSchned1 <romschana@gmail.com>
2022-01-25 15:04:37 +0100

    side/new

--- scriptfile.js
+++ scriptfile.js
@@ -6,5 +6,5 @@ for (let i = 0; i <= 8; i++) {
     if(i%3==0){
         getPicFromHTML.innerHTML += `<br>`;
     }
-    getPicFromHTML.innerHTML += `<img class="picturesSettings" src="./img/see${i+1}.jpg" alt="picture"></img>`;
+    getPicFromHTML.innerHTML += `<img class="picturesSettings" src="./img/seen${i+1}.jpg" alt="picture"></img>`;
 }
