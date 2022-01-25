2966b88d67f7ed28e47e5c616e5dcf1de28e80f0
romanaSchned1 <romschana@gmail.com>
2022-01-25 14:28:26 +0100

    new/site

---
+++ scriptfile.js
@@ -0,0 +1,10 @@
+let getPicFromHTML = document.getElementById('content');
+let count = 0;
+
+
+for (let i = 0; i <= 8; i++) {
+    if(i%3==0){
+        getPicFromHTML.innerHTML += `<br>`;
+    }
+    getPicFromHTML.innerHTML += `<img class="picturesSettings" src="./img/see${i+1}.jpg" alt="picture"></img>`;
+}
